029e146ac4bcced27fc7d49340ab7ca1a0d8ba09
Pranaw <wow@wow.wow>
2019-09-27 20:04:14 -0700

    Robot and Main TeleOpMode classes restructured for readability.

--- TeamCode/src/main/java/MainTeleOpMode.java
+++ TeamCode/src/main/java/MainTeleOpMode.java
@@ -2,47 +2,63 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import static com.sun.tools.doclint.Entity.lt;
+/*
+    Class: MainTeleOpMode
+
+    Access: Public
+
+    Purpose: To allow for the drivers to control the
+    robot during the player control portion
+
+    Arguments: None
+
+    Outputs: None
+ */
 
 @TeleOp(name = "TeleOp", group = "Tele OpMode")
 public class MainTeleOpMode extends TeleOpMode {
 
 
     @Override
-    public void run() {
-        // this runs when you tap the play button
-        this.wait(1.0);
+    public void run()
+    {
+        /*
+        Notes: This only runs on initialization. It's purpose is to clean up
+        the portions of the autonomous phase that couldn't be dealt with until
+        afterwards
+        */
         this.getRobot().getServo1().setPosition(Constants.Rest_Pos);
         this.getRobot().getColorSensor2().enableLed(false);
         this.getRobot().getColorSensor3().enableLed(false);
     }
 
     @Override
-    public void repeat() {
-        double lx = this.getGamepadA().left_stick_x;
-        double ly = this.getGamepadA().left_stick_y;
-        double rx = -this.getGamepadA().right_stick_x;
-        double ry = this.getGamepadA().right_stick_y;
-        double rt = this.getGamepadA().right_trigger;
-        double lt = this.getGamepadA().left_trigger;
-        double ly2 = this.getGamepadB().left_stick_y;
-        double ry2 = this.getGamepadB().right_stick_y;
+    public void repeat()
+    {
+        /*
+        Note: Declaration of all variables/ Linking the gamepads to the code
+        */
+            double lx = this.getGamepadA().left_stick_x;
+            double ly = this.getGamepadA().left_stick_y;
+            double rx = -this.getGamepadA().right_stick_x;
+            double ry = this.getGamepadA().right_stick_y;
+            double rt = this.getGamepadA().right_trigger;
+            double lt = this.getGamepadA().left_trigger;
+            double ly2 = this.getGamepadB().left_stick_y;
+            double ry2 = this.getGamepadB().right_stick_y;
 
-        boolean rb = this.getGamepadA().right_bumper;
-        boolean lb = this.getGamepadA().left_bumper;
-        boolean lb2 = this.getGamepadB().left_bumper;
-        boolean rb2 = this.getGamepadB().right_bumper;
+            boolean rb = this.getGamepadA().right_bumper;
+            boolean lb = this.getGamepadA().left_bumper;
+            boolean lb2 = this.getGamepadB().left_bumper;
+            boolean rb2 = this.getGamepadB().right_bumper;
 
-        boolean open = this.getGamepadA().y;
-        boolean close = this.getGamepadA().x;
-        double pos = this.getRobot().getServo1().getPosition();
+            boolean open = this.getGamepadA().y;
+            boolean close = this.getGamepadA().x;
+            double pos = this.getRobot().getServo1().getPosition();
 
-        double gate_pos = this.getRobot().getServo2().getPosition();
-        double gate_open = this.getGamepadB().left_trigger;
-        double gate_close = this.getGamepadB().right_trigger;
-
-
-
-
+            double gate_pos = this.getRobot().getServo2().getPosition();
+            double gate_open = this.getGamepadB().left_trigger;
+            double gate_close = this.getGamepadB().right_trigger;
 
         /*
         Gamepad: Gamepad A
@@ -53,47 +69,46 @@ public class MainTeleOpMode extends TeleOpMode {
 
         */
 
-        //Claw
-        /*
-        if(open)
-        {
-            this.getRobot().getServo1().setPosition(Constants.Close_Pos);
-        }
-        else if(close) {
-            this.getRobot().getServo1().setPosition(Constants.Rest_Pos);
-        }
-        else {
-            this.getRobot().getServo1().setPosition(pos);
-        }
-    */
-
-        //Drive Base momvent
-        if(Math.abs(ry) >= Constants.TRIGGER_THRESHOLD || Math.abs(ly) >= Constants.TRIGGER_THRESHOLD) {
-            this.getRobot().getMotor1().setPower(-ry);
-            this.getRobot().getMotor2().setPower(ly);
-            }
-            else if (Math.abs(ry) <= Constants.TRIGGER_THRESHOLD || Math.abs(ly) <= Constants.TRIGGER_THRESHOLD)
-                {
-                    this.getRobot().getMotor1().setPower(-ry);
-                    this.getRobot().getMotor2().setPower(ly);
-
-        }
-        else{
-            this.getRobot().getMotor1().reset();
-            this.getRobot().getMotor2().reset();
-        }
-
-        //Lift
-        if(open){
-                this.getRobot().getMotor3().setPower(1);
-            }
-            else if(close){
-                this.getRobot().getMotor3().setPower(-1);
-            }
-            else
+            //Claw
+            if(open)
             {
-                this.getRobot().getMotor3().reset();
+                this.getRobot().getServo1().setPosition(Constants.Close_Pos);
             }
+            else if(close) {
+                this.getRobot().getServo1().setPosition(Constants.Rest_Pos);
+            }
+            else {
+                this.getRobot().getServo1().setPosition(pos);
+            }
+
+
+            //Drive Base movement
+            if(Math.abs(ry) >= Constants.TRIGGER_THRESHOLD || Math.abs(ly) >= Constants.TRIGGER_THRESHOLD) {
+                this.getRobot().getMotor1().setPower(-ry);
+                this.getRobot().getMotor2().setPower(ly);
+                }
+                else if (Math.abs(ry) <= Constants.TRIGGER_THRESHOLD || Math.abs(ly) <= Constants.TRIGGER_THRESHOLD)
+                    {
+                        this.getRobot().getMotor1().setPower(-ry);
+                        this.getRobot().getMotor2().setPower(ly);
+
+            }
+            else{
+                this.getRobot().getMotor1().reset();
+                this.getRobot().getMotor2().reset();
+            }
+
+            //Lift
+            if(open){
+                    this.getRobot().getMotor3().setPower(1);
+                }
+                else if(close){
+                    this.getRobot().getMotor3().setPower(-1);
+                }
+                else
+                {
+                    this.getRobot().getMotor3().reset();
+                }
 
 
 
@@ -106,53 +121,53 @@ public class MainTeleOpMode extends TeleOpMode {
             Bucket to Heaven
         */
 
-        //Bucket to Heaven
-        if(lb2){
-            this.getRobot().getCRservo1().setDirection(DcMotorSimple.Direction.FORWARD);
-            this.getRobot().getCRservo1().setPower(1);
-        }
-        else if(rb2){
-            this.getRobot().getCRservo1().setPower(-1);
-        }
-        else {
-            this.getRobot().getCRservo1().setPower(0);
-        }
+            //Bucket to Heaven
+            if(lb2){
+                this.getRobot().getCRservo1().setDirection(DcMotorSimple.Direction.FORWARD);
+                this.getRobot().getCRservo1().setPower(1);
+            }
+            else if(rb2){
+                this.getRobot().getCRservo1().setPower(-1);
+            }
+            else {
+                this.getRobot().getCRservo1().setPower(0);
+            }
 
-        //Stairway to Heaven
-        if(ly2 > Constants.TRIGGER_THRESHOLD ){
-            this.getRobot().getMotor4().setPower(ly2);
-        }
-        else if(ly2 < -Constants.TRIGGER_THRESHOLD){
-            this.getRobot().getMotor4().setPower(ly2);
-        }
-        else {
-            this.getRobot().getMotor4().setPower(0);
-        }
+            //Stairway to Heaven
+            if(ly2 > Constants.TRIGGER_THRESHOLD ){
+                this.getRobot().getMotor4().setPower(ly2);
+            }
+            else if(ly2 < -Constants.TRIGGER_THRESHOLD){
+                this.getRobot().getMotor4().setPower(ly2);
+            }
+            else {
+                this.getRobot().getMotor4().setPower(0);
+            }
 
-        //Ladder to Heaven
-        if(ry2 > Constants.TRIGGER_THRESHOLD ){
-            this.getRobot().getMotor5().setPower(ry2);
-        }
-        else if(ry2 < -Constants.TRIGGER_THRESHOLD){
-            this.getRobot().getMotor5().setPower(ry2);
-        }
-        else {
-            this.getRobot().getMotor5().setPower(0);
-        }
+            //Ladder to Heaven
+            if(ry2 > Constants.TRIGGER_THRESHOLD ){
+                this.getRobot().getMotor5().setPower(ry2);
+            }
+            else if(ry2 < -Constants.TRIGGER_THRESHOLD){
+                this.getRobot().getMotor5().setPower(ry2);
+            }
+            else {
+                this.getRobot().getMotor5().setPower(0);
+            }
 
-        //Gate
+            //Gate
 
-        if(gate_open > Constants.TRIGGER_THRESHOLD || gate_close < Constants.TRIGGER_THRESHOLD)
-        {
-            this.getRobot().getCRServo2().setPower(gate_open);
-        }
-        else if (gate_close > Constants.TRIGGER_THRESHOLD){
-            this.getRobot().getCRServo2().setPower(-gate_close);
-        }
-        else
-        {
-            this.getRobot().getCRServo2().setPower(0);
-        }
+            if(gate_open > Constants.TRIGGER_THRESHOLD || gate_close < Constants.TRIGGER_THRESHOLD)
+            {
+                this.getRobot().getCRServo2().setPower(gate_open);
+            }
+            else if (gate_close > Constants.TRIGGER_THRESHOLD){
+                this.getRobot().getCRServo2().setPower(-gate_close);
+            }
+            else
+            {
+                this.getRobot().getCRServo2().setPower(0);
+            }
 
     }
 
--- TeamCode/src/main/java/Robot.java
+++ TeamCode/src/main/java/Robot.java
@@ -7,254 +7,254 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 public class Robot {
     private ElapsedTime timer = new ElapsedTime(); // strictly for the wait function!
 
-    // wheel motors
-    private Motor m1 = null;
-    private Motor m2 = null;
-    private Motor m3 = null;
-    private Motor m4 = null;
-    private Motor m5 = null;
+    //DECLARATIONS OF HARDWARE/LINKAGE TO SOFTWARE
+            //Declarations of the motor objects
+                private Motor m1 = null;
+                private Motor m2 = null;
+                private Motor m3 = null;
+                private Motor m4 = null;
+                private Motor m5 = null;
 
 
-    // servos
-    private Servo s1;
-    private Servo s2;
-    private Servo s3;
+            //Declarations of the servo objects
+                private Servo s1;
+                private Servo s2;
+                private Servo s3;
 
-    private CRServo crs1;
-    private CRServo crs2;
-
-
-    //Sensors
-
-    private ColorSensor cs1 = null;
-    private ColorSensor cs2 = null;
-    private ColorSensor cs3 = null;
+            //Declarations of the crservo objects
+                private CRServo crs1;
+                private CRServo crs2;
 
 
-
-    private OpMode mode = null;
-
-    /* robot constructor, initialize stuff */
-    public Robot(OpMode mode) {
-        this.mode = mode;
-
-        // create motor&servo instances
-        this.m1 = new Motor(this, this.getOpMode().getMotor(Constants.M1_MOTOR));
-        this.m2 = new Motor(this, this.getOpMode().getMotor(Constants.M2_MOTOR));
-        this.m3 = new Motor(this, this.getOpMode().getMotor(Constants.M3_MOTOR));
-        this.m4 = new Motor(this, this.getOpMode().getMotor(Constants.M4_MOTOR));
-        this.m5 = new Motor(this, this.getOpMode().getMotor(Constants.M5_Motor));
-        this.s1 = new Servo(this, this.getOpMode().getServo(Constants.S1_Servo));
-        this.s2 = new Servo(this, this.getOpMode().getServo(Constants.S2_Servo));
-        this.s3 = new Servo(this, this.getOpMode().getServo(Constants.S3_Servo));
-        this.crs1 = new CRServo(this,this.getOpMode().getCRServo(Constants.CR1_CRservo));
-        this.crs2 = new CRServo(this,this.getOpMode().getCRServo(Constants.CR2_CRservo));
-
-       // this.m5 = new Motor(this, this.getOpMode().getMotor(Constants.M5_MOTOR));
-        // this.s1 = new Servo(this, this.getOpMode().getServo(Constants.S1_SERVO)); // uncomment to enable servo
-        // this.s1.setCenter(0.5);
-
-        // enable/disable encoders on motors
-        this.getMotor1().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.getMotor2().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.getMotor3().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.getMotor4().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            //Declarations of the colorsensor objects
+                private ColorSensor cs1 = null;
+                private ColorSensor cs2 = null;
+                private ColorSensor cs3 = null;
 
 
-        // define sensors
-        cs1 = this.getOpMode().getHardwareMap().get(ColorSensor.class, "color1");
-        cs2 = this.getOpMode().getHardwareMap().get(ColorSensor.class, "color2");
-        cs3 = this.getOpMode().getHardwareMap().get(ColorSensor.class, "color3");
+            //Declaration of an Opmode Object
+            private OpMode mode;
+
+            /*
+            Constructor
+
+            Purpose: To connect the Robot Class to the Opmode class and the actual robot hardware.
+            This allows any function of the robot to be controlled through the Robot Class
+
+            Arguments: mode (Opmode)
+            */
+            public Robot(OpMode mode) {
+                this.mode = mode;
+
+                //Create motor & servo instances
+                this.m1 = new Motor(this, this.getOpMode().getMotor(Constants.M1_MOTOR));
+                this.m2 = new Motor(this, this.getOpMode().getMotor(Constants.M2_MOTOR));
+                this.m3 = new Motor(this, this.getOpMode().getMotor(Constants.M3_MOTOR));
+                this.m4 = new Motor(this, this.getOpMode().getMotor(Constants.M4_MOTOR));
+                this.m5 = new Motor(this, this.getOpMode().getMotor(Constants.M5_Motor));
+                this.s1 = new Servo(this, this.getOpMode().getServo(Constants.S1_Servo));
+                this.s2 = new Servo(this, this.getOpMode().getServo(Constants.S2_Servo));
+                this.s3 = new Servo(this, this.getOpMode().getServo(Constants.S3_Servo));
+                this.crs1 = new CRServo(this,this.getOpMode().getCRServo(Constants.CR1_CRservo));
+                this.crs2 = new CRServo(this,this.getOpMode().getCRServo(Constants.CR2_CRservo));
+
+                //Enable/disable encoders on motors
+                this.getMotor1().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                this.getMotor2().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                this.getMotor3().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                this.getMotor4().setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+                //Definition of color sensors
+                cs1 = this.getOpMode().getHardwareMap().get(ColorSensor.class, "color1");
+                cs2 = this.getOpMode().getHardwareMap().get(ColorSensor.class, "color2");
+                cs3 = this.getOpMode().getHardwareMap().get(ColorSensor.class, "color3");
 
 
 
-    }
-
-
-
-    /* drive for x seconds at x speed */
-    /*public void drive(double speed, double time) {
-        setPower(speed);
-
-        this.wait(time);
-
-        reset(); // TODO: make only main motors stop
-    }*/
-
-    /* reset all motors */
-    public void reset() {
-        this.getMotor1().reset();
-        this.getMotor2().reset();
-        this.getMotor3().reset();
-        this.getMotor4().reset();
-        this.getMotor5().reset();
-        this.getServo3().reset();
-        this.getServo1().reset();
-        //this.getMotor5().reset();
-    }
-
-
-    /* get motors */
-    public Motor getMotor1() {
-        return this.m1;
-    }
-    public Motor getMotor2() {
-        return this.m2;
-    }
-    public Motor getMotor3() {
-        return this.m3;
-    }
-    public Motor getMotor4() {
-        return this.m4;
-    }
-    public Motor getMotor5() { return this.m5;}
-   /* public Motor getMotor5() {
-        return this.m5;
-    }
-
-    /* get servos */
-    public Servo getServo1() {return this.s1;}
-    public Servo getServo2() {return this.s2;}
-    public Servo getServo3() {return this.s3;}
-
-    public CRServo getCRservo1(){return this.crs1;}
-    public CRServo getCRServo2(){return this.crs2;}
-
-    /* get Opmode */
-    public OpMode getOpMode() {
-        return this.mode;
-    }
-
-    /* get color sensor */
-    public ColorSensor getColorSensor1() { return this.cs1; }
-    public ColorSensor getColorSensor2() { return this.cs2; }
-    public ColorSensor getColorSensor3() { return this.cs3; }
-
-
-    /* wait x seconds */
-    public void wait(double seconds) {
-        timer.reset();
-        while(timer.seconds() < seconds);
-    }
-
-
-    /*
-    Landing Function (Autonomous)
-    Arguments:
-        Power(double: Sets power to motors (preset direction)
-        Time(double): Amount of time motors should have power
-    Task:
-        Executes the Landing maneuver.
-        1)Brings robot down by extending arm(motor 3) for an amount of time
-        2)Rotates in the right direction (topside view)
-    */
-    public void Land( double power, double time) {
-        this.getMotor3().setPower(-power);
-        this.wait(1.0 * time);
-        this.getMotor3().setPower(0);
-        this.wait(0.2);
-    }
-
-    /*
-    Left Rotate Function (Topside View)
-    Arguments:
-        Speed: Speed of rotation
-        Time: Time which robot will rotate.
-    Task:
-        Rotates left
-    */
-    public void LRot ( double speed, double time)
-    {
-        this.getMotor1().setPower(speed);
-        this.getMotor2().setPower(speed);
-        this.wait(time);
-        this.getMotor1().setPower(0);
-        this.getMotor2().setPower(0);
-        this.wait(0.2);
-    }
-
-    /*
-     Rotate Right Function (Topside View)
-     Arguments:
-         Speed: Speed of rotation
-         Time: Time which robot will rotate.
-     Task:
-         Rotates right
-     */
-    public void RRot ( double speed, double time)
-    {
-        this.getMotor1().setPower(-speed);
-        this.getMotor2().setPower(-speed);
-        this.wait(time);
-        this.getMotor1().setPower(0);
-        this.getMotor2().setPower(0);
-        this.wait(0.2);
-    }
-
-    /*
-     Forward Function (Topside View)
-     Arguments:
-         Speed: Speed of movement
-         Time: Time which robot will move forward
-     Task:
-         Goes forward
-     */
-    public void Forward (double speed, double time)
-    {
-        this.getMotor1().setPower(speed);
-        this.getMotor2().setPower(-speed);
-        this.wait(time);
-        this.getMotor1().setPower(0);
-        this.getMotor2().setPower(0);
-        this.wait(0.2);
-    }
-
-    /*
-    Backward Function (Topside View)
-    Arguments:
-        Speed: Speed of movement
-        Time: Time which robot will move backward
-    Task:
-        Goes backward
-    */
-    public void Backward (double speed, double time)
-    {
-        this.getMotor1().setPower(-speed);
-        this.getMotor2().setPower(speed);
-        this.wait(time);
-        this.getMotor1().setPower(0);
-        this.getMotor2().setPower(0);
-        this.wait(0.2);
-    }
-
-
-    /*
-    Realignment Forward Function
-    Arguments:
-        Color: Of side
-
-    Task:
-        Correct to fit lines based n color
-    */
-    public void RealignmentBlue ( double power){
-        while(this.getColorSensor2().blue() < 4 || this.getColorSensor3().blue() < 4) {
-            if (this.getColorSensor2().blue() < 4) {
-                this.getMotor1().setPower(power / 2);
-            } else if (this.getColorSensor3().blue() < 4) {
-                this.getMotor2().setPower(-power / 2);
             }
-            else {
-                this.getMotor1().setPower(0);
-                this.getMotor2().setPower(0);
+
+
+            //Reset all motors
+                public void reset() {
+                this.getMotor1().reset();
+                this.getMotor2().reset();
+                this.getMotor3().reset();
+                this.getMotor4().reset();
+                this.getMotor5().reset();
+                this.getServo3().reset();
+                this.getServo1().reset();
+                //this.getMotor5().reset();
             }
-            this.getColorSensor2().blue();
-            this.getColorSensor3().blue();
-        }
-        this.getMotor1().setPower(0);
-        this.getMotor1().setPower(0);
-        this.wait(0.2);
-
-    }
 
 
+            //Returns motor objects
+                public Motor getMotor1() {
+                    return this.m1;
+                }
+                public Motor getMotor2() {
+                    return this.m2;
+                }
+                public Motor getMotor3() {
+                    return this.m3;
+                }
+                public Motor getMotor4() {
+                    return this.m4;
+                }
+                public Motor getMotor5() { return this.m5; }
+
+
+            //Returns servo objects
+                public Servo getServo1() {return this.s1;}
+                public Servo getServo2() {return this.s2;}
+                public Servo getServo3() {return this.s3;}
+
+            //Returns crservo objects
+            public CRServo getCRservo1(){return this.crs1;}
+            public CRServo getCRServo2(){return this.crs2;}
+
+            //Returns Opmode
+                public OpMode getOpMode()
+                {
+                    return this.mode;
+                }
+
+            //Returns Color sensor objects
+            public ColorSensor getColorSensor1() { return this.cs1; }
+            public ColorSensor getColorSensor2() { return this.cs2; }
+            public ColorSensor getColorSensor3() { return this.cs3; }
+
+
+            //Causes the code to wait
+                public void wait(double seconds) {
+                    timer.reset();
+                    while(timer.seconds() < seconds);
+                }
+
+    /*
+    AUTONOMOUS COMMANDS
+
+    Purpose: These allow Autonomous to be coded simply without needing
+    to write where every piece of electricity needs to go
+     */
+
+            /*
+            Landing Function (Autonomous)
+            Arguments:
+                Power(double: Sets power to motors (preset direction)
+                Time(double): Amount of time motors should have power
+            Task:
+                Executes the Landing maneuver.
+                1)Brings robot down by extending arm(motor 3) for an amount of time
+                2)Rotates in the right direction (topside view)
+            */
+                    public void Land( double power, double time)
+                    {
+                        this.getMotor3().setPower(-power);
+                        this.wait(1.0 * time);
+                        this.getMotor3().setPower(0);
+                        this.wait(0.2);
+                    }
+
+            /*
+            Left Rotate Function (Topside View)
+            Arguments:
+                Speed: Speed of rotation
+                Time: Time which robot will rotate.
+            Task:
+                Rotates left
+            */
+                    public void LRot ( double speed, double time)
+                    {
+                        this.getMotor1().setPower(speed);
+                        this.getMotor2().setPower(speed);
+                        this.wait(time);
+                        this.getMotor1().setPower(0);
+                        this.getMotor2().setPower(0);
+                        this.wait(0.2);
+                    }
+
+            /*
+             Rotate Right Function (Topside View)
+             Arguments:
+                 Speed: Speed of rotation
+                 Time: Time which robot will rotate.
+             Task:
+                 Rotates right
+             */
+                    public void RRot ( double speed, double time)
+                    {
+                        this.getMotor1().setPower(-speed);
+                        this.getMotor2().setPower(-speed);
+                        this.wait(time);
+                        this.getMotor1().setPower(0);
+                        this.getMotor2().setPower(0);
+                        this.wait(0.2);
+                    }
+
+            /*
+             Forward Function (Topside View)
+             Arguments:
+                 Speed: Speed of movement
+                 Time: Time which robot will move forward
+             Task:
+                 Goes forward
+             */
+                    public void Forward (double speed, double time)
+                    {
+                        this.getMotor1().setPower(speed);
+                        this.getMotor2().setPower(-speed);
+                        this.wait(time);
+                        this.getMotor1().setPower(0);
+                        this.getMotor2().setPower(0);
+                        this.wait(0.2);
+                    }
+
+            /*
+            Backward Function (Topside View)
+            Arguments:
+                Speed: Speed of movement
+                Time: Time which robot will move backward
+            Task:
+                Goes backward
+            */
+                    public void Backward (double speed, double time)
+                    {
+                        this.getMotor1().setPower(-speed);
+                        this.getMotor2().setPower(speed);
+                        this.wait(time);
+                        this.getMotor1().setPower(0);
+                        this.getMotor2().setPower(0);
+                        this.wait(0.2);
+                    }
+
+
+            /*
+            Realignment Forward Function
+            Arguments:
+                Color: Of side
+
+            Task:
+                Correct to fit lines based n color
+            */
+                    public void RealignmentBlue ( double power)
+                    {
+                        while(this.getColorSensor2().blue() < 4 || this.getColorSensor3().blue() < 4) {
+                            if (this.getColorSensor2().blue() < 4) {
+                                this.getMotor1().setPower(power / 2);
+                            } else if (this.getColorSensor3().blue() < 4) {
+                                this.getMotor2().setPower(-power / 2);
+                            }
+                            else {
+                                this.getMotor1().setPower(0);
+                                this.getMotor2().setPower(0);
+                            }
+                            this.getColorSensor2().blue();
+                            this.getColorSensor3().blue();
+                        }
+                        this.getMotor1().setPower(0);
+                        this.getMotor1().setPower(0);
+                        this.wait(0.2);
+
+                    }
 }
